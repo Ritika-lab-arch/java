f17c73ad412cd412426c405b914f5d49b366a1d3
Ritika-lab-arch <vishwakarmaritika905@gmail.com>
2025-04-09 13:58:41 +0530

    java

--- index.java
+++ index.java
@@ -1,5 +1,32 @@
-public class helloworld { 
-    public static void main (string[] args){
-        system.err.println(10+10+"hello");
+
+import javax.sound.sampled.SourceDataLine;
+
+public class index{
+    public static void main(String[] args) {
+        // our first program 
+        int a=7;
+        switch (a) {
+        case 1:
+            System.out.println("Monday");
+            break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+                 case 3:
+                    System.out.println("wednesday");
+                    break;
+                     case 4:
+                        System.out.println("thursday");
+                        break;
+                        case 5:
+                            System.out.println("friday");
+                            break;
+                            case 6:
+                                System.out.println("saturday");
+                                case 7:
+                                    System.out.println("sunday");
+                                    break;
+                                    default:
+        }
     }
 }
